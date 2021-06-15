fca48ce39097fe59e1207c317e97636d295dfdea
jpatemb <51932270+jpatemb@users.noreply.github.com>
2021-06-15 12:02:30 -0400

    PythonCalculator
    Contains code for Python Calculator app.

---
+++ PythonProgram/PythonProgram.py
@@ -0,0 +1,31 @@
+print("Hello and welcome to the Python Calculator application. We currently support addition, subtraction, multiplication, and division for integer values.\n")
+
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+
+sum = float(num1) + float(num2)
+
+print("The sum of your first and second number is {0}".format(sum))
+
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+
+diff = float(num1) - float(num2)
+
+print("The difference between your first and second number is {0}".format(diff))
+
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+
+prod = float(num1) * float(num2)
+
+print("The product of your first and second number is {0}".format(prod))
+
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+
+quot = float(num1) / float(num2)
+
+print("The quotient of your first and second number is {0}\n".format(quot))
+
+print("Thanks for trying out the Python Calculator App. We hope to see you back here again. Have a great day!\n")
